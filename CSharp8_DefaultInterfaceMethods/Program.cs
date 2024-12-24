78e7e6b3f0eb94122e6d369d761d7e1f08c66ff0
Aleksejs Birula <ice19942335@gmail.com>
2024-12-24 12:58:30 +0200

    Init

---
+++ CSharp8_DefaultInterfaceMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+IDefaultInterfaceMethod anyClass = new AnyClass();
+anyClass.DefaultMethod();
+Console.ReadKey();
+
+public interface IDefaultInterfaceMethod
+{
+    public const int Number = 100;
+    
+    public void DefaultMethod()
+    {
+        Console.WriteLine($"I am default method in the interface! My number is: {Number}");
+    }
+}
+
+public class AnyClass : IDefaultInterfaceMethod
+{
+}
---
+++ src/CSharp_8/CSharp8_ReadonlyStruct/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Rectangle rectangle = new Rectangle(10, 20);
+Console.WriteLine("Height: " + rectangle.Height);
+Console.WriteLine("width: " + rectangle.Width);
+Console.WriteLine("Rectangle Area: " + rectangle.Area);
+Console.WriteLine("Rectangle: " + rectangle);
+Console.ReadKey();
+
+// rectangle.Height = rectangle.Height / 2; //Compiling error
+
+public readonly struct Rectangle
+{
+    public double Height { get; }
+    public double Width { get; }
+    public double Area => Height * Width;
+
+    public Rectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public override string ToString()
+    {
+        return $"(Total area for height: {Height}, width: {Width} is {Area})";
+    }
+}
