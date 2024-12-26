221312d99598815a11855888f01b26ae14e4a789
Aleksejs Birula <ice19942335@gmail.com>
2024-12-26 17:51:11 +0200

    Property patterns

---
+++ src/CSharp_8/EnhancedPatternMatching/CSharp8_PropertyPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Rectangle rectangle = new Rectangle { Length = 5, Height = 10 };
+IsSpecificRectanglee(rectangle);
+
+void IsSpecificRectanglee(Rectangle rect)
+{
+    if (rect is Rectangle { Length: 5 or 10, Height: 10 or 20 } specificRectangle)
+    {
+        Console.WriteLine($"Rectangle Length: {specificRectangle.Length} and Height: {specificRectangle.Height}");
+    }
+}
+
+class Rectangle
+{
+    public double Length { get; set; }
+    public double Height { get; set; }
+}
