9b7dc52d8c3dd8f6d547de6c189a78483bcca925
Aleksejs Birula <ice19942335@gmail.com>
2024-12-29 14:36:26 +0200

    Local functions

---
+++ src/CSharp_8/CSharp8_LocalFunctions/Program.cs
@@ -0,0 +1,44 @@
+﻿// This particular example is made specifically for - Local functions and exceptions - To demonstrate that:
+// One of the useful features of local functions is that they can allow exceptions to surface immediately.
+// For iterator methods, exceptions are surfaced only when the returned sequence is enumerated, and not when the iterator is retrieved.
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        IEnumerable<int> xs = OddSequence(50, 110);
+        Console.WriteLine("Retrieved enumerator...");
+
+        foreach (var x in xs)
+        {
+            Console.Write($"{x} ");
+        }
+    }
+
+    // One of the useful features of local functions is that they can allow exceptions to surface immediately.
+    private static IEnumerable<int> OddSequence(int start, int end)
+    {
+        if (start < 0 || start > 99)
+            throw new ArgumentOutOfRangeException(nameof(start), "start must be between 0 and 99.");
+        if (end > 100)
+            throw new ArgumentOutOfRangeException(nameof(end), "end must be less than or equal to 100.");
+        if (start >= end)
+            throw new ArgumentException("start must be less than end.");
+            
+        return GetOddSequenceEnumerator();
+
+        
+        IEnumerable<int> GetOddSequenceEnumerator()
+        {
+            for (var i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ src/CSharp_8/CSharp8_LocalFunctions/README.md
@@ -0,0 +1,7 @@
+﻿### Local functions (C# Programming Guide)
+
+This particular example is made specifically for: Local functions and exceptions
+To demonstrate that:
+> One of the useful features of local functions is that they can allow exceptions to surface immediately. For iterator methods, exceptions are surfaced only when the returned sequence is enumerated, and not when the iterator is retrieved. For async methods, any exceptions thrown in an async method are observed when the returned task is awaited.
+
+More info here: https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/local-functions
