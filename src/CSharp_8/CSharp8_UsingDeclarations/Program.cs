dc9fc0080159451aff47e9333a8550c957dba883
Aleksejs Birula <ice19942335@gmail.com>
2024-12-29 14:06:00 +0200

    using declarations

---
+++ src/CSharp_8/CSharp8_UsingDeclarations/Program.cs
@@ -0,0 +1,46 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/using
+
+using System.Runtime.CompilerServices;
+
+var cancellationToken = CancellationToken.None;
+var path = Path.Combine(Environment.CurrentDirectory, "Text.txt");
+
+await foreach (var number in GetNumbers(path, cancellationToken))
+{
+    Console.WriteLine($"{DateTime.Now:hh:mm:ss.fff}: {number}");
+}
+
+return;
+
+static async IAsyncEnumerable<int> GetNumbers(string filePath, [EnumeratorCancellation] CancellationToken cancellationToken)
+{
+    using StreamReader reader = File.OpenText(filePath);
+
+    while (await reader.ReadLineAsync(cancellationToken) is { } line)
+    {
+        if (int.TryParse(line, out int number))
+        {
+            await Task.Delay(1000, cancellationToken);
+            yield return number;
+        }
+    }
+
+    // When declared in a using declaration, a local variable is disposed at the end of the scope in which it's declared.
+    // In the preceding example, disposal happens at the end of a method.
+}
+
+static async Task<int> GetNumber(string[] filePaths, CancellationToken cancellationToken)
+{
+    using StreamReader reader = File.OpenText(filePaths[0]);
+    using StreamReader reader1 = File.OpenText(filePaths[1]);
+
+    var line = await reader.ReadLineAsync(cancellationToken);
+    if (line is not null && int.TryParse(line, out int number))
+    {
+        return number;
+    }
+
+    return 0;
+    
+    // When you declare several instances in one using statement, they are disposed in reverse order of declaration.
+}
---
+++ src/CSharp_8/CSharp8_UsingDeclarations/README.md
@@ -0,0 +1,2 @@
+﻿### using statement - ensure the correct use of disposable objects
+> https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/using
