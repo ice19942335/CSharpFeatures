2b6e1821e990d5713abc32e7b60c3cefd9d2c137
Aleksejs Birula <ice19942335@gmail.com>
2025-01-25 14:38:19 +0200

    Constraint unmanaged

---
+++ src/CSharp8/UnmanagedConstructedTypes/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace UnmanagedConstructedTypes;
+
+class MyCustomDict<TKey, TValue>
+    where TKey : struct
+    where TValue : unmanaged
+{
+    private Dictionary<TKey, TValue> _dict = new();
+
+    public TValue this[TKey index]
+    {
+        get => _dict[index];
+        set => _dict[index] = value;
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        _dict.Add(key, value);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, _dict.Select(x => $"Key: {x.Key}, Value: {x.Value}"));
+    }
+}
+
+struct Key
+{
+    public string Name { get; init; }
+
+    public override string ToString() => Name;
+}
+
+struct ValidUnmanagedValue // Satisfies unmanaged constraint https://stackoverflow.com/questions/55016755/difference-between-unmanaged-and-struct-constraints-in-c-sharp-generics
+{
+    public int Id { get; init; }
+    public int Permission { get; init; }
+
+    public override string ToString() => $"Id: {Id} - Permission: {Permission}";
+}
+
+struct InvalidUnmanagedValue // Does not satisfy unmanaged constraint https://stackoverflow.com/questions/55016755/difference-between-unmanaged-and-struct-constraints-in-c-sharp-generics
+{
+    public string Name { get; set; }
+    public int[] Permissions { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InvalidUsageOfUnmanagedConstraint();
+        ValdUsageOfUnmanagedConstraint();
+    }
+
+    private static void ValdUsageOfUnmanagedConstraint()
+    {
+        // Will compile =) All constraints are satisfied.
+        var dict = new MyCustomDict<Key, ValidUnmanagedValue>();
+        dict.Add(new Key { Name = "Key1" }, new ValidUnmanagedValue { Id = 1, Permission = 1 });
+        Console.WriteLine(dict);
+    }
+
+    private static void InvalidUsageOfUnmanagedConstraint()
+    {
+        // Will not compile as we're trying to apply invalid unmanaged struct.
+        // var dict = new MyCustomDict<Key, InvalidUnmanagedValue>();
+    }
+}
