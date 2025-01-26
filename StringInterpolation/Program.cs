8e951c01a47d29f2e58d5f023c1b0f1fb9ca7ee8
Aleksejs Birula <ice19942335@gmail.com>
2025-01-26 14:03:01 +0200

    CSharp 8 - String interpolation.

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StringInterpolation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = "John";
+        var date = DateTime.Now;
+        
+        // Composite formatting:
+        Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfWeek, date);
+        
+        // String interpolation:
+        Console.WriteLine($"Hello, {name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
+        
+        // The following example uses optional formatting components described in the preceding table:
+        Console.WriteLine($"|{"Align Left", -20}|{"Align Right", 20}|");
+    }
+}
